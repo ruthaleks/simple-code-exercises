9e74f4e5015250888e7f63c7e5bb2606cae1ac56
Ruth Blankfors <ruth.aleks@gmail.com>
2024-08-05 12:36:39 +0200

    Solution to exercise two

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn exercise_one() {
     match median_result {
         Some(value) => { 
             let median = value;
-            println!("Median of the list {:?} is {median}", list);
+            println!("Median of the list {list:?} is {median}");
             match max.value {
                 Some(val) => println!("Most occuring value in the list is {val}"),
                 None => println!(" Something vent wrong, no values in the list")
@@ -51,6 +51,43 @@ fn exercise_one() {
 
 }
 
+fn is_vowel(c: &char) -> bool {
+    matches!(c, 'a'| 'e' | 'o' | 'i' | 'u' | 'A'| 'E' | 'O' | 'I' | 'U')
+}
+
+fn is_consonant(c: &char) -> bool {
+    c.is_alphabetic() & !is_vowel(c)
+}
+
+fn exercise_two() {
+    // Convert strings to pig latin. The first consonant of each word 
+    // is moved to the end of the word and ay is added, so first becomes 
+    // irst-fay. Words that start with a vowel have hay added to the end 
+    // instead (apple becomes apple-hay). Keep in mind the details about 
+    // UTF-8 encoding!
+
+    let word = String::from("apple");
+    let mut pig_latin_word: Option<String> = None;
+
+    for (idx, c) in word.chars().enumerate() {
+        if is_consonant(&c) {
+            let s = &word[idx+1..];
+            pig_latin_word = Some(format!("{s}-{c}ay"));
+            break;  
+        }
+        else if (idx == 0) & is_vowel(&c) {
+            pig_latin_word = Some(format!("{word}-hay"));
+            break;
+              
+        }
+    }
+    match pig_latin_word {
+        Some(pig_latin) => println!("The word: {word} converted to pig latin: {pig_latin}"),
+        None => println!("Unable to convert the word: {word} to pig latin")
+    }
+}
+
 fn main() {
     exercise_one();
+    exercise_two();
 }
